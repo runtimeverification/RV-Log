858789b5ff3f99a9257de447cc3b7acf85a95f76
xiaohe27 <xiaoguoyi27@gmail.com>
2015-06-09 17:06:14 -0500

    run.sh script works

--- src/main/scripts/run.sh
+++ src/main/scripts/run.sh
@@ -2,26 +2,13 @@
 #The second argument is the relative path to the specification file. 
 #The options -events, -witness and -trace can be given afterwards in any order.
 
-RVM_ARGS=""
-for var in "$@"
-do
-   if ["$var" == "-events"]
-   then RVM_ARGS="$RVM_ARGS $var"
+TraceFile=$1
+SpecFile=$2
+shift;
+shift;
 
-   elif ["$var" == "-witness"]
-   then RVM_ARGS="$RVM_ARGS $var"
-
-   elif ["$var" == "-trace"]
-   then RVM_ARGS="$RVM_ARGS $var"
-   
-   else 
-	:
-   fi
-   
-done
-
-rv-log $2
-rv-monitor -t -d CustomizedLogReader/rvm/ $RVM_ARGS --indexByVal $2
+rv-log $SpecFile
+rv-monitor -t -d CustomizedLogReader/rvm/ --indexByVal $@ $SpecFile
 javac CustomizedLogReader/rvm/*.java
 cd CustomizedLogReader/
-java rvm.LogReader ../$1
+java rvm.LogReader ../$TraceFile
