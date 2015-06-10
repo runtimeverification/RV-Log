640317cd1418e08c51def4dfb540c3dd3abb6b47
xiaohe27 <hexiao2@illinois.edu>
2015-06-09 20:46:22 -0500

    update script

--- src/main/scripts/run.sh
+++ src/main/scripts/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# The below incantation sets SRC_ROOT to be the canonicalized directory of this script
+SRC_ROOT=$(
+cd -P -- "$(dirname -- "$0")" &&
+printf '%s\n' "$(pwd -P)/$(basename -- "$0")"
+) || exit 1
+SRC_ROOT=`dirname "$SRC_ROOT"`
+
+RELEASE="$SRC_ROOT/rvmLib"
+export CLASSPATH="$RELEASE/rv-monitor.jar:$RELEASE/rv-monitor-rt.jar:$CLASSPATH"
+
 #The first argument is the path (relative to the location of this script) to the trace file.
 #The second argument is the relative path to the specification file. 
 #The options -events, -witness and -trace can be given afterwards in any order.
