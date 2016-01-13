7f759da91bb42c74cfb082086ee2cde9e1539507
xiaohe27 <xiaoguoyi27@gmail.com>
2016-01-12 23:19:19 -0600

    add some test infrastructure

---
+++ src/test/java/HasNextTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by hx312 on 1/12/2016.
+ */
+public class HasNextTest {
+    private TestHelper testHelper;
+    @Test
+    public void test_RVM_Availability() {
+    }
+}
